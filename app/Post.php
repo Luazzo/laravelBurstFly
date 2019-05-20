d9bb8d6f313a0c859e8b8438f511556f60b012ac
Irina <i.tsimpilova@gmail.com>
2019-05-21 00:51:47 +0200

    feat : modelesMig finished

--- app/Post.php
+++ app/Post.php
@@ -5,8 +5,15 @@ namespace App;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Post
+ * @package App
+ */
 class Post extends \TCG\Voyager\Models\Post
 {
+	/**
+	 * @param $slug
+	 */
 	public static function postsOfCategory($slug)
 	{
 		/*return $this  ->where('categories.slug','=', $slug)

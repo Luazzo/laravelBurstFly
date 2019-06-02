8ea5b1ad2993b5ebeff8fe0b92abceb9cb295df3
Irina <i.tsimpilova@gmail.com>
2019-06-02 17:23:05 +0200

    feat: finish fronend perfection

--- resources/views/comment/_commentsPost.blade.php
+++ resources/views/comment/_commentsPost.blade.php
@@ -5,7 +5,7 @@
         @else
             <div class="post-reply-2">
         @endif
-                <div class="image-reply-post"><img src="{{ Voyager::image( $comment->user->avatar ) }}" alt="comment"></div>
+                <div class="image-reply-post" style="background: url({{ Voyager::image( $comment->user->avatar ) }}) no-repeat; background-size: 100%; "></div>
                 <div class="name-reply-post-2">{{$comment->user->name}}</div>
                 <div class="text-reply-post-2">{{$comment->body}}</div>
             </div>
